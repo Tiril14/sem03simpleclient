f6fb446cd8fd43c684d690962ff664ed4a2a1251
tirilkleppe <tirilk@uia.no>
2023-04-24 09:33:53 +0200

    client go

--- tcp_client.go
+++ tcp_client.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"github.com/Tiril14/is105sem03/mycrypt"
 	"log"
 	"net"
 	"os"
-
-	"github.com/Tiril14/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.4:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println("os.Args[1] = ", os.Args[1])
 
-	_, err = conn.Write([]byte(os.Args[1]))
+	// Encrypt the message
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,11 +27,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
 
-	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-	log.Println("Kryptert melding: ", string(kryptertMelding))
-	_, err = conn.Write([]byte(string(kryptertMelding)))
+	// Decrypt the response from the server
+	deCrypt := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Println("Dekrypter melding: ", string(deCrypt))
+
+	response := string(deCrypt)
+	log.Printf("reply from proxy: %s", response)
 
 }
